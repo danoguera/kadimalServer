c97129537121fa95aa9f140eb703a3b0816bcfeb
Diego <danoguera@gmail.com>
2020-07-29 15:26:12 -0500

    Add database connection

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 function initDatabase(){
-    const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-    mongoose.connect(process.env.MONGO_URL, options);
-    const { connection } = mongoose;
-    connection.once("open", () => console.log(`DB Connection established..`));
-    connection.on("error", (err) => console.log("Error: ", err));
-    return connection;
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  mongoose.connect(process.env.MONGO_URL, options);
+  const { connection } = mongoose;
+  connection.once("open", () => console.log(`DB Connection established..`));
+  connection.on("error", (err) => console.log("Error: ", err));
+  return connection;
 }
 
 module.exports = initDatabase;
